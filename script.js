b9bc68ce54254bd9c026a58b6bc7bd83d1d8308c
Kevin Garcia Martin <kevingm@live.se>
2024-09-23 14:02:47 +0200

    Add footer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector("#menu-btn");
 const navLinks = document.querySelectorAll(".nav-link");
+const fastTravel = document.querySelector("#fast-travel");
 
 menuBtn.addEventListener("click", () => {
   const header = document.querySelector("header");
@@ -9,8 +10,17 @@ menuBtn.addEventListener("click", () => {
 
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
-    const activeNavLink = document.getElementsByClassName("nav-link active");
-    activeNavLink[0].classList.remove("active");
+    removeNavLinkActiveState();
     navLink.classList.add("active");
   });
 });
+
+fastTravel.addEventListener("click", () => {
+  removeNavLinkActiveState();
+  navLinks[0].classList.add("active");
+});
+
+function removeNavLinkActiveState() {
+  const activeNavLink = document.getElementsByClassName("nav-link active");
+  activeNavLink[0].classList.remove("active");
+}
