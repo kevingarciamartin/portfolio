406d6d87bc43635c908b19d1c6ece21674043600
Kevin Garcia Martin <kevingm@live.se>
2024-09-27 10:52:33 +0200

    Add live clock to header

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const header = document.querySelector("header");
 const menuBtn = document.querySelector("#menu-btn");
+const clockHours = document.querySelector(".hours");
+const clockColon = document.querySelector(".colon");
+const clockMinutes = document.querySelector(".minutes");
 const navLinks = document.querySelectorAll(".nav-link");
 const fastTravel = document.querySelector("#fast-travel");
 
@@ -26,6 +29,27 @@ fastTravel.addEventListener("click", () => {
   removeNavLinkActiveState();
   navLinks[0].classList.add("active");
 });
+
+updateClock();
+animateClock();
+
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  clockHours.textContent = `(${hours.toString().padStart(2, "0")}`;
+  clockColon.textContent = ":";
+  clockMinutes.textContent = `${minutes.toString().padStart(2, "0")})`;
+
+  setTimeout(updateClock, 60 * 1000);
+}
+
+function animateClock() {
+  setInterval(() => {
+    clockColon.style.opacity = clockColon.style.opacity === "1" ? "0" : "1";
+  }, 1000);
+}
 
 function removeNavLinkActiveState() {
   const activeNavLink = document.getElementsByClassName("nav-link active");
