8bb2136acb269717e9cc31fba8d03c1ac76190de
Kevin Garcia Martin <kevingm@live.se>
2024-12-28 09:04:11 -0500

    Update clock to always show the time of the location I'm based in

--- src/assets/js/modules/clock.js
+++ src/assets/js/modules/clock.js
@@ -1,12 +1,16 @@
+import { toZonedTime } from "date-fns-tz";
+
 export const clock = (() => {
   const clockHours = document.querySelector(".hours");
   const clockColon = document.querySelector(".colon");
   const clockMinutes = document.querySelector(".minutes");
 
   function updateClock() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
+    const userLocalTime = new Date();
+    const kevinTimeZone = "Europe/Stockholm";
+    const kevinTime = toZonedTime(userLocalTime, kevinTimeZone);
+    const hours = kevinTime.getHours();
+    const minutes = kevinTime.getMinutes();
 
     clockHours.textContent = `(${hours.toString().padStart(2, "0")}`;
     clockColon.textContent = ":";
