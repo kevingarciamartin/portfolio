0ec63372521368a723d6fd1cf34691560a310e35
Kevin Garcia Martin <kevingm@live.se>
2025-01-08 15:02:16 +0100

    Render work item with more details when clicked on

--- src/assets/js/pages/work/work.js
+++ src/assets/js/pages/work/work.js
@@ -1,6 +1,36 @@
 import "./work.css";
+import { scrollToTop, trimStartOfString } from "../../utils/helpers";
 import doremir from "../../../img/doremir_logo.png";
 import chess50 from "../../../img/chess50.png";
+
+const workArray = [
+  {
+    name: "Doremir Music Research",
+    role: "Internship",
+    stack: ["React"],
+    description: [
+      "Developed the company React dashboard for internal statistics by continously fetching new relevant data from their API and implementing new pages, charts and tables.",
+      "Analyzed the UI of one of the company's products for Windows and implemented the corresponding design in React for an upcoming hybrid application. In conjuction, I did a refactoring of the project where I removed Bootstrap, to more easily be able to implement the specific design from the computer version.",
+    ],
+    image: doremir,
+    url: "https://doremir.com/",
+    urlShowcase: "doremir.com",
+  },
+  {
+    name: "Chess50",
+    stack: ["Flask", "Python", "JavaScript", "SQLite"],
+    description: [
+      "A mini-clone application of the popular website chess.com. Features inlude:",
+      "- playing chess against an engine (Stockfish 17) with take-back moves, engine moves on demand and flipping the board,",
+      "- solving chess.com's Daily Puzzle,",
+      "- keeping track of the leaderboards for the most popular time controls,",
+      "- and searching for valid chess.com usernames to get their profile information and game statistics.",
+    ],
+    image: chess50,
+    url: "https://chess50-dde8edd06871.herokuapp.com/",
+    urlShowcase: "chess50-dde8edd06871.herokuapp.com",
+  },
+];
 
 export function work() {
   const main = document.querySelector("main");
@@ -12,7 +42,7 @@ export function work() {
 
   workArray.forEach((element, index) => {
     const item = document.createElement("li");
-    const link = document.createElement("a");
+    const content = document.createElement("div");
     const image = document.createElement("img");
     const lineAfter = document.createElement("div");
 
@@ -22,45 +52,126 @@ export function work() {
       item.appendChild(lineBefore);
     }
 
+    content.classList.add("work__content");
     lineAfter.classList.add("horizontal-line", "after");
 
-    link.setAttribute("href", element.url);
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener noreferrer");
-
-    link.innerHTML = `
+    content.innerHTML = `
       <div class="work__info">
         <span></span>
         <span>${element.name}</span>
-        <svg width="80px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg width="80px" height="80px" viewBox="0 -0.5 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="si-glyph si-glyph-arrow-right">
+            <title>1178</title>  
+            <defs>
+        </defs>
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <path d="M6.077,1.162 C6.077,1.387 6.139,1.612 6.273,1.812 L10.429,8.041 L6.232,14.078 C5.873,14.619 6.019,15.348 6.56,15.707 C7.099,16.068 7.831,15.922 8.19,15.382 L12.82,8.694 C13.084,8.3 13.086,7.786 12.822,7.39 L8.233,0.51 C7.873,-0.032 7.141,-0.178 6.601,0.181 C6.26,0.409 6.077,0.782 6.077,1.162 L6.077,1.162 Z" fill="#434343" class="si-glyph-fill">
+        </path>
+            </g>
         </svg>
+        <span></span>
+        <span class="work__stack">${
+          element.role ? element.role : element.stack.join(", ")
+        }</span>
       </div>
     `;
 
     image.src = element.image;
 
     list.appendChild(item);
-    item.appendChild(link);
+    item.appendChild(content);
     item.appendChild(lineAfter);
-    link.appendChild(image);
+    content.appendChild(image);
+
+    item.addEventListener("click", () => {
+      renderWorkItem(element);
+    });
   });
 
   main.appendChild(section);
   section.appendChild(list);
 }
 
-const workArray = [
-  {
-    name: "Doremir Music Research",
-    stack: ["React"],
-    image: doremir,
-    url: "https://doremir.com/",
-  },
-  {
-    name: "Chess50",
-    stack: ["Flask", "Python", "JavaScript", "SQLite"],
-    image: chess50,
-    url: "https://chess50-dde8edd06871.herokuapp.com/",
-  },
-];
+function renderWorkItem(item) {
+  hideContent();
+  scrollToTop();
+
+  const main = document.querySelector("main");
+  const workItem = document.createElement("section");
+
+  workItem.id = "work-item";
+
+  workItem.innerHTML = `
+  <div class="work-item__content">
+    <a href="${item.url}" target="_blank" rel="noopener noreferrer">
+      <img src="${item.image}">
+    </a>
+    <div class="work-item__title">
+      <p>${item.name}</p>
+      <span class="work-item__role"></span>
+    </div>
+    <div class="work-item__stack">
+      <span>Tech stack</span>
+      <p>${item.stack.join(", ")}</p>
+    </div>
+    <div class="work-item__description">
+      <span>Description</span>
+    </div>
+    <div class="work-item__url">
+      <span>URL</span>
+      <a href="${item.url}" target="_blank" rel="noopener noreferrer">${
+    item.urlShowcase
+  }</a>
+    </div>
+    <button class="close-btn">Go back</button>
+  </div>
+  `;
+
+  main.appendChild(workItem);
+
+  const workItemRole = document.querySelector(".work-item__role");
+  if (item.role === undefined) workItemRole.style.display = "none";
+  else workItemRole.textContent = item.role;
+
+  const workItemDescription = document.querySelector(".work-item__description");
+  let isStartedList = false;
+  item.description.forEach((paragraph) => {
+    let p;
+    if (paragraph.startsWith("- ")) {
+      if (!isStartedList) {
+        const ul = document.createElement("ul");
+        ul.classList.add("work-item__ul");
+        workItemDescription.appendChild(ul);
+
+        isStartedList = true;
+      }
+
+      const workItemUL = document.querySelector(".work-item__ul");
+      p = document.createElement("li");
+      workItemUL.appendChild(p);
+      paragraph = trimStartOfString(paragraph, "- ");
+    } else {
+      p = document.createElement("p");
+
+      isStartedList = false;
+    }
+
+    p.textContent = paragraph;
+    !isStartedList ? workItemDescription.appendChild(p) : "";
+  });
+
+  const closeBtn = workItem.querySelector(".close-btn");
+  closeBtn.addEventListener("click", () => {
+    workItem.remove();
+    hideContent(false);
+  });
+}
+
+function hideContent(isTrue = true) {
+  const work = document.querySelector("#work");
+  const contact = document.querySelector("section.contact");
+  const footer = document.querySelector("footer");
+
+  work.style.display = isTrue ? "none" : "block";
+  contact.style.display = isTrue ? "none" : "flex";
+  footer.style.display = isTrue ? "none" : "grid";
+}
