9ecdef8b69b69f3586267aac9e2e8ce5e679d4ce
Kevin Garcia Martin <kevingm@live.se>
2025-01-13 21:52:51 +0100

    Add navigation to home page when clicking mobile logo

--- src/assets/js/modules/navigation.js
+++ src/assets/js/modules/navigation.js
@@ -6,6 +6,7 @@ import { scrollToTop, hideMainWorkContent } from "../utils/helpers";
 export const handleNavigation = (() => {
   const header = document.querySelector("header");
   const navLinks = document.querySelectorAll(".nav-link");
+  const mobileLogo = document.querySelector("#mobile-logo");
   const fastTravel = document.querySelector("#fast-travel");
 
   navLinks.forEach((navLink) => {
@@ -22,29 +23,40 @@ export const handleNavigation = (() => {
           break;
         case "nav-work":
           pages.renderPage(work, "nav-work");
+          break;
         case "nav-contact":
           const workItem = document.querySelector("#work-item");
           if (workItem) {
             workItem.remove();
             hideMainWorkContent(false);
           }
+          break;
         default:
           break;
       }
     });
   });
 
+  mobileLogo.addEventListener("click", () => {
+    pages.renderPage(home, "nav-home");
+    scrollToTop();
+    switchNavLinkActiveState();
+  });
+
   fastTravel.addEventListener("click", () => {
     scrollToTop("smooth");
-
-    const currentNavIndex = pages.getIndexOfCurrentPage();
-
-    removeNavLinkActiveState();
-    navLinks[currentNavIndex].classList.add("active");
+    switchNavLinkActiveState();
   });
 
   function removeNavLinkActiveState() {
     const activeNavLink = document.getElementsByClassName("nav-link active");
     activeNavLink[0].classList.remove("active");
+  }
+
+  function switchNavLinkActiveState() {
+    const currentNavIndex = pages.getIndexOfCurrentPage();
+
+    removeNavLinkActiveState();
+    navLinks[currentNavIndex].classList.add("active");
   }
 })();
