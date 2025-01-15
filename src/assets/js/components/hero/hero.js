f6630cd601f80e6a396920dc985428d547bb0e10
Kevin Garcia Martin <kevingm@live.se>
2025-01-15 15:30:34 +0100

    Fix content jump on initial load

--- src/assets/js/components/hero/hero.js
+++ src/assets/js/components/hero/hero.js
@@ -4,15 +4,15 @@ import portrait from "../../../img/portrait.jpeg";
 export function hero() {
   const main = document.querySelector("main");
   const section = document.createElement("section");
-  const image = document.createElement("img");
+  const image = document.createElement("div");
   const title = document.createElement("h1");
 
   section.id = "hero";
   title.classList.add("hero__title");
 
-  image.setAttribute("alt", "Portrait of Kevin Garcia Martin");
-
-  image.src = portrait;
+  image.innerHTML = `
+    <img src="${portrait}" alt="${"Portrait of Kevin Garcia Martin"}">
+  `;
   title.textContent = "Kevin Garcia Martin";
 
   main.appendChild(section);
