f5256a0a65d05b8f3a972f53545d361b8519ee8e
Kevin Garcia Martin <kevingm@live.se>
2026-03-30 16:16:06 +0200

    refactor: migrate next.config.js to next.config.ts and update images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
   compiler: {
     styledComponents: true,
   },
