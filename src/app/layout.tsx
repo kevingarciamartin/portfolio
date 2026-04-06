7b18c59a62b9a00c2aa8dd248be3110d65747074
Kevin Garcia Martin <kevingm@live.se>
2026-04-06 16:40:39 +0200

    fix: update favicon paths in site manifest and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,29 @@
 import ClientLayout from "@/components/ClientLayout";
-import "@/styles/fonts.css";
 import "@/styles/reset.css";
+import "@/styles/fonts.css";
 import "@/styles/style.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kevin Garcia Martin — Developer x Engineer",
+  description: "Personal Portfolio of Kevin Garcia Martin",
+  icons: {
+    icon: [
+      {
+        url: "/img/favicon_io/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/img/favicon_io/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: "/img/favicon_io/apple-touch-icon.png",
+  },
+  manifest: "/img/favicon_io/site.webmanifest",
+};
 
 export default function RootLayout({
   children,
